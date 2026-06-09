142704b00e164292e11227cfeb82bc07cc958a23
Kitos <kitos@MacBook-Air-Kitos.local>
2026-06-09 22:38:27 +0300

    add logging for debug

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!! VITE_API_URL = ${VITE_API_URL:-<not set>}"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Starting frontend..."
 exec make start
